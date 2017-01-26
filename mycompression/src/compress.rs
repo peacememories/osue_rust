1ff7b143742d1e8d8c27451464ac00908b328315
Gabriel Pickl <peacemotion@gmail.com>
2017-01-26 01:11:02 +0100

    Changed compress structure.

--- mycompression/src/compress.rs
+++ mycompression/src/compress.rs
@@ -1,32 +1,32 @@
 use std::io::Read;
+use std::iter::Peekable;
 
-struct Compressor<C, I> {
-    last_char: Option<C>,
-    iter: I
+struct Compressor<I: Iterator> {
+    iter: Peekable<I>
 }
 
-impl <C: PartialEq<C> + Clone, I: Iterator<Item=C>> Iterator for Compressor<C, I> {
+impl <C: PartialEq, I: Iterator<Item=C>> Iterator for Compressor<I> {
     type Item = (usize, C);
 
     fn next(&mut self) -> Option<(usize, C)> {
-        let mut count = 1;
-        for new_val in self.iter.by_ref() {
-            match self.last_char {
-                Some(ref old_val) if &new_val == old_val => {
-                    count = count+1;
+        let mut count = 0;
+        loop {
+            match self.iter.next() {
+                Some(value) => {
+                    match self.iter.peek() {
+                        Some(new_value) if new_value == &value => {
+                            count = count + 1;
+                        },
+                        _ => {
+                            return Some((count+1, value))
+                        }
+                    }
                 },
-                Some(_) => {
-                    self.last_char = Some(new_val.clone());
-                    return Some((count, new_val));
-                }
                 None => {
-                    self.last_char = Some(new_val);
+                    break
                 }
             }
         }
-        match self.last_char {
-            Some(ref old_char) => Some((count, old_char.clone())),
-            None => None
-        }
+        None
     }
 }
