7361f7105a2262244a1448c0de41a062253e6553
Gabriel Pickl <peacemotion@gmail.com>
2017-01-26 01:23:59 +0100

    Added trait implementations for compress.

--- mycompression/src/compress.rs
+++ mycompression/src/compress.rs
@@ -1,13 +1,14 @@
 use std::io::Read;
 use std::iter::Peekable;
 
-struct Compressor<I: Iterator> {
+pub struct Compressor<I: Iterator> {
     iter: Peekable<I>
 }
 
 impl <C: PartialEq, I: Iterator<Item=C>> Iterator for Compressor<I> {
     type Item = (usize, C);
 
+    #[inline]
     fn next(&mut self) -> Option<(usize, C)> {
         let mut count = 0;
         loop {
@@ -28,5 +29,20 @@ impl <C: PartialEq, I: Iterator<Item=C>> Iterator for Compressor<I> {
             }
         }
         None
+    }
+}
+
+pub trait IntoCompressor: Iterator
+    where Self: Sized
+{
+    fn compress(self) -> Compressor<Self>;
+}
+
+impl <C: PartialEq, T: Iterator<Item=C>> IntoCompressor for T {
+    #[inline]
+    fn compress(self) -> Compressor<Self> {
+        Compressor {
+            iter: self.peekable()
+        }
     }
 }
