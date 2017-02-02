9757d5d810a2e6e2f9406e6d77cd6a21891551c3
Gabriel Pickl <peacemotion@gmail.com>
2017-02-02 15:42:39 +0100

    Changed mycompression design.

--- mycompression/src/bin/mycompress.rs
+++ mycompression/src/bin/mycompress.rs
@@ -1,60 +1,38 @@
-extern crate getopts;
-extern crate multi_reader;
+extern crate mycompression;
 
-use getopts::Options;
-use std::borrow::Cow;
-use std::fs::File;
 use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::env;
-use multi_reader::MultiReader;
+use std::io::Read;
+use std::io::Write;
+use mycompression::Compressor;
+use mycompression::Counter;
 
-macro_rules! panic_userfacing {
-    ($($stuff:tt)*) => {
-        {
-            use std::io::Write;
-            let _ = writeln!(::std::io::stderr(), $($stuff)*);
-            ::std::process::exit(1);
-        }
-    }
+struct Stat {
+    read: usize,
+    written: usize
 }
 
-fn usage(program_name: &str, opts: &Options) -> String {
-    format!("{} [infile...]", opts.short_usage(program_name))
+fn compress<R: Read, W: Write>(reader: R, writer: W) -> io::Result<Stat> {
+    let mut read = 0;
+    let mut counter = Counter::new(writer);
+    {
+        let mut compressor = Compressor::new(&mut counter);
+        for res in reader.bytes() {
+            let byte = try!(res);
+            read += 1;
+            try!(compressor.write(&[byte]));
+        }
+        try!(compressor.flush());
+    }
+    Ok(Stat{
+        read: read,
+        written: counter.written()
+    })
 }
 
 fn main() {
-    let mut args = env::args();
-    let program_name = args.next()
-        .map(Cow::from)
-        .unwrap_or("mycompression".into());
-
-    let mut opts = Options::new();
-
-    opts.optopt("o", "", "Write output to a file", "outfile");
-    
-    let matches = opts.parse(args).unwrap_or_else(
-        |err| {
-            panic_userfacing!("{}\n{}", err, usage(&program_name, &opts));
-        }
-    );
-
-    let infiles = matches.free
-        .into_iter()
-        .map(File::open)
-        .collect::<Result<Vec<File>,_>>()
-        .unwrap_or_else(
-            |err| {
-                panic_userfacing!("Failed to open file: {}", err);
-            }
-        );
-
-    if infiles.is_empty() {
-        
-    } else {
-        
-    }
-
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let res = compress(stdin.lock(), stdout.lock()).unwrap();
+    println!("");
+    println!("{}, {}", res.read, res.written);
 }
--- mycompression/src/lib.rs
+++ mycompression/src/lib.rs
@@ -1,1 +1,81 @@
-mod compress;
+use std::io::Write;
+use std::io;
+
+pub struct Compressor<W> {
+    last: Option<(usize, u8)>,
+    writer: W
+}
+
+impl <W: Write> Compressor<W> {
+    pub fn new(writer: W) -> Self {
+        Compressor {
+            last: None,
+            writer: writer
+        }
+    }
+
+    fn write_comp(&mut self, amount: usize, byte: u8) -> io::Result<()> {
+        try!(write!(self.writer, "{}", amount));
+        self.writer.write_all(&[byte])
+    }
+}
+
+impl <W: Write> Write for Compressor<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        for byte in buf {
+            match self.last.take() {
+                Some((amount, old_byte)) if byte == &old_byte => {
+                    self.last = Some((amount+1, old_byte));
+                },
+                Some((amount, old_byte)) => {
+                    self.last = Some((1, byte.clone()));
+                    try!(self.write_comp(amount, old_byte));
+                },
+                None => {
+                    self.last = Some((1, byte.clone()));
+                }
+            }
+        }
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match self.last.take() {
+            Some((amount, byte)) => {
+                try!(self.write_comp(amount, byte));
+            },
+            _ => {}
+        }
+        self.writer.flush()
+    }
+}
+
+pub struct Counter<T> {
+    writer: T,
+    written: usize
+}
+
+impl <T: Write> Counter<T> {
+    pub fn new(writer: T) -> Self {
+        Counter {
+            writer: writer,
+            written: 0
+        }
+    }
+
+    pub fn written(&self) -> usize {
+        self.written
+    }
+}
+
+impl <T: Write> Write for Counter<T> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let written = try!(self.writer.write(buf));
+        self.written += written;
+        Ok(written)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
